75e5e0e0d7c9fa13d228bd1105de148a7b6ac1a3
neuremberg <neur.matos@gmail.com>
2020-06-29 12:03:35 -0300

    Atualizando paths dos dados

--- processamento-dados/rearrange-data-economatica.R
+++ processamento-dados/rearrange-data-economatica.R
@@ -4,24 +4,27 @@ read_data <- function(path, sheet){
   readxl::read_xlsx(path = path, sheet = sheet, na = '-', skip = 3)
 }
 
-
 write_data <- function(data, path){
   write.csv2(x = data, file = path, row.names = FALSE, na = '')
 }
 
 
 # Read data
-path_in <- "dados/Economatica-2009-2020.xlsx"
+path_in <- "dados/bruto/Economatica-2009-2020.xlsx"
+path_out <- "dados/processado/"
+
+# Nomes planilhas
 sheets <- readxl::excel_sheets(path_in)
 
+# Carregando dados planilhas
 data_ <- lapply(
   X = sheets,
   FUN = read_data,
   path = path_in
 )
 
-# Export data 
-paths_out <- paste('resultados/', sheets, '.csv', sep = '')
+# Export data para csv 
+paths_out <- paste(path_out, sheets, '.csv', sep = '')
 mapply(
   FUN = write_data,
   data = data_,
